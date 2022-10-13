9e0954fb72ca1308f791eb76cd1a0541a18c2c36
djatwz <85258031+djatwz@users.noreply.github.com>
2022-10-12 21:09:51 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var puntos=0;
+$(function(){
+	$(".items").hover(function(){
+		var tis = $(this);
+		tis.find(".hueco").click(function(){
+			if(tis.hasClass("active")){
+			tis.removeClass("active");
+			puntos++;
+			$("#puntos").html(puntos);
+
+			}
+			else {
+				console.log(":/");
+			}
+		})
+	})
+})
+var iterval = setInterval(function(){
+	topoaleatorio();
+},700)
+
+topoaleatorio = function(){
+	var num = parseInt(Math.random() * 12);
+	$(".items").removeClass("active");
+	$(".items:nth-child("+num+")").addClass("active");
+}
